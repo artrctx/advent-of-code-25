57d8b5d499d791c705348a03e1285b855ccd3f24
artrctx <artrctx@gmail.com>
2025-12-07 17:42:01 -0600

    d7

--- internal/d7/d7.go
+++ internal/d7/d7.go
@@ -2,6 +2,7 @@ package d7
 
 import (
 	"fmt"
+	"slices"
 
 	"github.com/artrctx/advent-of-code-25/internal/input"
 )
@@ -37,34 +38,30 @@ func Part1() {
 }
 
 func Part2() {
-	rows := input.GetRows("./internal/d7/example.txt")
-	var answer uint
+	rows := input.GetRows("./internal/d7/input.txt")
 
 	rLen := len(rows[0])
-
-	beams := rows[0]
+	sIdx := slices.Index(rows[0], 'S')
+	beams := slices.Replace(make([]uint, rLen), sIdx, sIdx+1, 1)
+	fmt.Println(beams)
 	for idx := 2; idx < rLen; idx += 2 {
 		for bIdx, b := range beams {
-			if b == '.' {
+			if b == 0 || rows[idx][bIdx] == '.' {
 				continue
 			}
-
-			if rows[idx][bIdx] == '.' {
-				continue
+			if idx == 3 {
+				fmt.Print()
 			}
 
-			beams[bIdx] = '.'
-			if bIdx > 0 {
-				beams[bIdx-1] = 'S'
-				answer++
-			}
-
-			if bIdx < rLen-1 {
-				beams[bIdx+1] = 'S'
-				answer++
-			}
-
+			beams[bIdx] = 0
+			beams[bIdx+1] += b
+			beams[bIdx-1] += b
 		}
+	}
+
+	var answer uint
+	for _, v := range beams {
+		answer += v
 	}
 
 	fmt.Printf("D7 Part 2: %v\n", answer)
